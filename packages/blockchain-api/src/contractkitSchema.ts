844c9b57496278a8c2cb804564f58fe90f0d4e1d
Nam Chu Hoai <nambrot@googlemail.com>
2020-06-03 11:44:40 +0200

    RG

--- packages/blockchain-api/src/contractkitSchema.ts
+++ packages/blockchain-api/src/contractkitSchema.ts
@@ -1,13 +1,38 @@
 import { newReleaseGold } from '@celo/contractkit/lib/generated/ReleaseGold'
 import { ReleaseGoldWrapper } from '@celo/contractkit/lib/wrappers/ReleaseGold'
 import { gql } from 'apollo-server-express'
+import { BigNumber } from 'bignumber.js'
 import { DataSources } from './apolloServer'
-
+function fromWei(num: BigNumber) {
+  return num.div(new BigNumber(10).exponentiatedBy(18))
+}
 export const typeDefs = gql`
   type ReleaseGoldContract {
     address: String!
     totalBalance: String!
     totalWithdrawn: String!
+    remainingLockedBalance: String!
+    remainingUnlockedBalance: String!
+    currentReleasedTotalAmount: String!
+    nonVotingLockedGold: String!
+    maxDistribution: String!
+    requiresDistribution: Boolean!
+    releaseSchedule: ReleaseSchedule!
+    votingRecord: [VotingRecord!]
+  }
+
+  type ReleaseSchedule {
+    amountReleasedPerPeriod: String!
+    releaseStartTime: Int!
+    releaseCliff: Int!
+    numReleasePeriods: Int!
+    releasePeriod: Int!
+  }
+
+  type VotingRecord {
+    activeVoteValue: String!
+    pendingVoteValue: String!
+    groupAddress: String!
   }
 
   type Query {
@@ -33,10 +58,56 @@ export const resolvers = {
           const kit = contractKit.kit
           const address = args.address as string
           const contract = new ReleaseGoldWrapper(kit, newReleaseGold(kit.web3, address))
+
+          const lockedgold = await kit.contracts.getLockedGold()
+          const election = await kit.contracts.getElection()
+
+          const [
+            totalWithdrawn,
+            totalBalance,
+            remainingUnlockedBalance,
+            remainingLockedBalance,
+            currentReleasedTotalAmount,
+            releaseSchedule,
+            nonVotingLockedGold,
+            votingRecord,
+            maxDistribution,
+          ] = await Promise.all([
+            contract.getTotalWithdrawn(),
+            contract.getTotalBalance(),
+            contract.getRemainingUnlockedBalance(),
+            contract.getRemainingLockedBalance(),
+            contract.getCurrentReleasedTotalAmount(),
+            contract.getReleaseSchedule(),
+            lockedgold.getAccountNonvotingLockedGold(address),
+            election.getVoter(address),
+            contract.getMaxDistribution(),
+          ])
+
+          const requiresDistribution = !maxDistribution.isEqualTo(
+            new BigNumber(2).exponentiatedBy(256).minus(1)
+          )
+
+          const { amountReleasedPerPeriod, ...restSchedule } = releaseSchedule
           return JSON.stringify({
             address,
-            totalBalance: await contract.getTotalBalance(),
-            totalWithdrawn: await contract.getTotalWithdrawn(),
+            totalWithdrawn: fromWei(totalWithdrawn).toString(),
+            totalBalance: fromWei(totalBalance).toString(),
+            remainingLockedBalance: fromWei(remainingLockedBalance).toString(),
+            remainingUnlockedBalance: fromWei(remainingUnlockedBalance).toString(),
+            currentReleasedTotalAmount: fromWei(currentReleasedTotalAmount).toString(),
+            releaseSchedule: {
+              amountReleasedPerPeriod: fromWei(amountReleasedPerPeriod).toString(),
+              ...restSchedule,
+            },
+            nonVotingLockedGold: fromWei(nonVotingLockedGold).toString(),
+            votingRecord: votingRecord.votes.map((_) => ({
+              activeVoteValue: fromWei(_.active).toString(),
+              pendingVoteValue: fromWei(_.pending).toString(),
+              groupAddress: _.group,
+            })),
+            maxDistribution: fromWei(maxDistribution).toString(),
+            requiresDistribution,
           })
         }
       )
