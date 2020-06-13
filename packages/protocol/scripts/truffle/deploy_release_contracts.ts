f1648fbbcc617dd40315cc6b75b5af68da2c8161
lucasege <lucas@celo.org>
2020-06-13 10:54:53 -0700

    Adding more confirmations

--- packages/protocol/scripts/truffle/deploy_release_contracts.ts
+++ packages/protocol/scripts/truffle/deploy_release_contracts.ts
@@ -20,8 +20,8 @@ let deployedGrantsFile: string
 let ReleaseGoldMultiSigProxy: ReleaseGoldMultiSigProxyContract
 let ReleaseGold: ReleaseGoldContract
 let ReleaseGoldProxy: ReleaseGoldProxyContract
-const ONE_CGLD = web3.utils.toWei('1', 'ether')
-const TWO_CGLD = web3.utils.toWei('2', 'ether')
+// const ONE_CGLD = web3.utils.toWei('1', 'ether')
+// const TWO_CGLD = web3.utils.toWei('2', 'ether')
 const MAINNET_START_TIME = new Date('22 April 2020 16:00:00 UTC').getTime() / 1000
 
 async function handleGrant(releaseGoldConfig: any, currGrant: number) {
@@ -111,7 +111,28 @@ async function handleGrant(releaseGoldConfig: any, currGrant: number) {
 
   // Reflect any rounding changes from the division above
   totalValue = adjustedAmountPerPeriod.multipliedBy(releaseGoldConfig.numReleasePeriods)
+  let response = await prompts({
+    type: 'confirm',
+    name: 'confirmation',
+    message,
+  })
 
+  if (!response.confirmation) {
+    console.info(chalk.yellow('Aborting due to user response'))
+    process.exit(0)
+  }
+
+  response = await prompts({
+    type: 'confirm',
+    name: 'confirmation',
+    message:
+      'About to initialize RG contract at proxy address ' + releaseGoldProxy.address + 'Ok? (y/n)',
+  })
+
+  if (!response.confirmation) {
+    console.info(chalk.yellow('Aborting due to user response'))
+    process.exit(0)
+  }
   const releaseGoldTxHash = await _setInitialProxyImplementation(
     web3,
     releaseGoldInstance,
@@ -137,17 +158,62 @@ async function handleGrant(releaseGoldConfig: any, currGrant: number) {
     '0x000000000000000000000000000000000000ce10'
   )
   const proxiedReleaseGold = await ReleaseGold.at(releaseGoldProxy.address)
+  response = await prompts({
+    type: 'confirm',
+    name: 'confirmation',
+    message:
+      'About to transfer the ownership of the underlying RG contract at ' +
+      proxiedReleaseGold.address +
+      '\nTo the ReleaseGoldMultiSigProxy at ' +
+      releaseGoldMultiSigProxy.address +
+      ' Ok? (y/n)',
+  })
+
+  if (!response.confirmation) {
+    console.info(chalk.yellow('Aborting due to user response'))
+    process.exit(0)
+  }
   await retryTx(proxiedReleaseGold.transferOwnership, [
     releaseGoldMultiSigProxy.address,
     {
       from: fromAddress,
     },
   ])
+  response = await prompts({
+    type: 'confirm',
+    name: 'confirmation',
+    message:
+      'About to transfer the ownership of the RG proxy at ' +
+      releaseGoldProxy.address +
+      '\nTo the ReleaseGoldMultiSigProxy at ' +
+      releaseGoldMultiSigProxy.address +
+      ' Ok? (y/n)',
+  })
+
+  if (!response.confirmation) {
+    console.info(chalk.yellow('Aborting due to user response'))
+    process.exit(0)
+  }
   await retryTx(releaseGoldProxy._transferOwnership, [
     releaseGoldMultiSigProxy.address,
     { from: fromAddress },
   ])
   // Send starting gold amount to the beneficiary so they can perform transactions.
+  response = await prompts({
+    type: 'confirm',
+    name: 'confirmation',
+    message:
+      'About to send the startGold amount ' +
+      startGold +
+      'cGLD to the beneficiary: ' +
+      releaseGoldConfig.beneficiary +
+      ' Ok? (y/n)',
+  })
+
+  if (!response.confirmation) {
+    console.info(chalk.yellow('Aborting due to user response'))
+    process.exit(0)
+  }
   await retryTx(web3.eth.sendTransaction, [
     {
       from: fromAddress,
@@ -174,99 +240,99 @@ async function handleGrant(releaseGoldConfig: any, currGrant: number) {
   fs.writeFileSync(argv.output_file, JSON.stringify(releases, null, 2))
 }
 
-async function checkBalance(releaseGoldConfig: any) {
-  const weiAmountReleasedPerPeriod = new BigNumber(
-    web3.utils.toWei(releaseGoldConfig.amountReleasedPerPeriod.toString())
-  )
-  const grantDeploymentCost = weiAmountReleasedPerPeriod
-    .multipliedBy(releaseGoldConfig.numReleasePeriods)
-    .plus(ONE_CGLD) // Tx Fees
-    .toFixed()
-  while (true) {
-    const fromBalance = new BigNumber(await web3.eth.getBalance(fromAddress))
-    if (fromBalance.gte(grantDeploymentCost)) {
-      break
-    }
-    console.info(
-      chalk.yellow(
-        fromAddress +
-          "'s balance will not cover the next grant with identifier " +
-          releaseGoldConfig.identifier +
-          '.'
-      )
-    )
-    const addressResponse = await prompts({
-      type: 'text',
-      name: 'newFromAddress',
-      message:
-        "If you would like to continue deployment from a new address please provide it now. If you would like to cancel deployment please enter 'exit'. Grants already deployed will persist. \
-        \nNote that the key for the current `from` address needs to remain accessible in order to roll funds over to the new `from` address.",
-    })
+// async function checkBalance(releaseGoldConfig: any) {
+//   const weiAmountReleasedPerPeriod = new BigNumber(
+//     web3.utils.toWei(releaseGoldConfig.amountReleasedPerPeriod.toString())
+//   )
+//   const grantDeploymentCost = weiAmountReleasedPerPeriod
+//     .multipliedBy(releaseGoldConfig.numReleasePeriods)
+//     .plus(ONE_CGLD) // Tx Fees
+//     .toFixed()
+//   while (true) {
+//     const fromBalance = new BigNumber(await web3.eth.getBalance(fromAddress))
+//     if (fromBalance.gte(grantDeploymentCost)) {
+//       break
+//     }
+//     console.info(
+//       chalk.yellow(
+//         fromAddress +
+//         "'s balance will not cover the next grant with identifier " +
+//         releaseGoldConfig.identifier +
+//         '.'
+//       )
+//     )
+//     const addressResponse = await prompts({
+//       type: 'text',
+//       name: 'newFromAddress',
+//       message:
+//         "If you would like to continue deployment from a new address please provide it now. If you would like to cancel deployment please enter 'exit'. Grants already deployed will persist. \
+//         \nNote that the key for the current `from` address needs to remain accessible in order to roll funds over to the new `from` address.",
+//     })
 
-    if (addressResponse.newFromAddress === 'exit') {
-      console.info('Exiting because of user input.')
-      process.exit(0)
-    }
+//     if (addressResponse.newFromAddress === 'exit') {
+//       console.info('Exiting because of user input.')
+//       process.exit(0)
+//     }
 
-    if (!web3.utils.isAddress(addressResponse.newFromAddress)) {
-      console.info(
-        chalk.red('Provided address is invalid, please insert a valid Celo Address.\nRetrying.')
-      )
-      continue
-    }
-    // Must be enough to handle 1cGLD test transfer and 1cGLD for transaction fees
-    if (fromBalance.gt(TWO_CGLD)) {
-      console.info(
-        '\nSending 1 cGLD as a test from ' +
-          fromAddress +
-          ' to ' +
-          addressResponse.newFromAddress +
-          ' to verify ownership.\n'
-      )
-      await retryTx(web3.eth.sendTransaction, [
-        {
-          from: fromAddress,
-          to: addressResponse.newFromAddress,
-          value: ONE_CGLD,
-        },
-      ])
-      const confirmResponse = await prompts({
-        type: 'confirm',
-        name: 'confirmation',
-        message:
-          'Please check the balance of your provided address. You should see the 1cGLD transfer and an initial genesis balance if this is a shard from the genesis block.\nCan you confirm this (y/n)?',
-      })
-      if (!confirmResponse.confirmation) {
-        console.info(chalk.red('Setting new address failed.\nRetrying.'))
-        continue
-      }
-      console.info(chalk.green('\nTransfer confirmed, sending the remaining balance.\n'))
-      const fromBalancePostTransfer = new BigNumber(await web3.eth.getBalance(fromAddress))
-      await retryTx(web3.eth.sendTransaction, [
-        {
-          from: fromAddress,
-          to: addressResponse.newFromAddress,
-          value: fromBalancePostTransfer.minus(ONE_CGLD).toFixed(), // minus Tx Fees
-        },
-      ])
-    }
-    const switchResponse = await prompts({
-      type: 'confirm',
-      name: 'confirmation',
-      message:
-        'Please now switch your local wallet from ' +
-        fromAddress +
-        ' to ' +
-        addressResponse.newFromAddress +
-        '.\nIf you respond `n` here the script will exit because balance has already been sent.\nPlease confirm when you are able to switch (y/n)?',
-    })
-    if (!switchResponse.confirmation) {
-      console.info(chalk.red('Switching wallets abandoned, exiting.'))
-      process.exit(0)
-    }
-    fromAddress = addressResponse.newFromAddress
-  }
-}
+//     if (!web3.utils.isAddress(addressResponse.newFromAddress)) {
+//       console.info(
+//         chalk.red('Provided address is invalid, please insert a valid Celo Address.\nRetrying.')
+//       )
+//       continue
+//     }
+//     // Must be enough to handle 1cGLD test transfer and 1cGLD for transaction fees
+//     if (fromBalance.gt(TWO_CGLD)) {
+//       console.info(
+//         '\nSending 1 cGLD as a test from ' +
+//         fromAddress +
+//         ' to ' +
+//         addressResponse.newFromAddress +
+//         ' to verify ownership.\n'
+//       )
+//       await retryTx(web3.eth.sendTransaction, [
+//         {
+//           from: fromAddress,
+//           to: addressResponse.newFromAddress,
+//           value: ONE_CGLD,
+//         },
+//       ])
+//       const confirmResponse = await prompts({
+//         type: 'confirm',
+//         name: 'confirmation',
+//         message:
+//           'Please check the balance of your provided address. You should see the 1cGLD transfer and an initial genesis balance if this is a shard from the genesis block.\nCan you confirm this (y/n)?',
+//       })
+//       if (!confirmResponse.confirmation) {
+//         console.info(chalk.red('Setting new address failed.\nRetrying.'))
+//         continue
+//       }
+//       console.info(chalk.green('\nTransfer confirmed, sending the remaining balance.\n'))
+//       const fromBalancePostTransfer = new BigNumber(await web3.eth.getBalance(fromAddress))
+//       await retryTx(web3.eth.sendTransaction, [
+//         {
+//           from: fromAddress,
+//           to: addressResponse.newFromAddress,
+//           value: fromBalancePostTransfer.minus(ONE_CGLD).toFixed(), // minus Tx Fees
+//         },
+//       ])
+//     }
+//     const switchResponse = await prompts({
+//       type: 'confirm',
+//       name: 'confirmation',
+//       message:
+//         'Please now switch your local wallet from ' +
+//         fromAddress +
+//         ' to ' +
+//         addressResponse.newFromAddress +
+//         '.\nIf you respond `n` here the script will exit because balance has already been sent.\nPlease confirm when you are able to switch (y/n)?',
+//     })
+//     if (!switchResponse.confirmation) {
+//       console.info(chalk.red('Switching wallets abandoned, exiting.'))
+//       process.exit(0)
+//     }
+//     fromAddress = addressResponse.newFromAddress
+//   }
+// }
 
 async function handleJSONFile(err, data) {
   if (err) {
@@ -384,7 +450,7 @@ async function handleJSONFile(err, data) {
     // Trim whitespace in case of bad copy/paste in provided json.
     releaseGoldConfig.beneficiary = releaseGoldConfig.beneficiary.trim()
     releaseGoldConfig.identifier = releaseGoldConfig.identifier.trim()
-    await checkBalance(releaseGoldConfig)
+    // await checkBalance(releaseGoldConfig)
     await handleGrant(releaseGoldConfig, currGrant)
     currGrant++
   }
