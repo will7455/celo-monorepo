954fdc23809d7e3696472cf08497b691cd40aa8e
Tarik Bellamine <tarikbellamine@protonmail.com>
2020-06-18 19:37:11 -0700

    working on single digit intputs

--- packages/mobile/src/components/SingleDigitInput.tsx
+++ packages/mobile/src/components/SingleDigitInput.tsx
@@ -1,18 +1,31 @@
-import TextInput from '@celo/react-components/components/TextInput'
+import TextInput from '@celo/react-components/components/TextInput.v2'
 import colors from '@celo/react-components/styles/colors'
 import { componentStyles } from '@celo/react-components/styles/styles'
 import * as React from 'react'
-import { StyleSheet } from 'react-native'
+import {
+  StyleSheet,
+  TextInput as RNTextInput,
+  TextInputProps as RNTextInputProps,
+} from 'react-native'
 
 export interface SingleDigitInputProps {
   inputValue: string
   inputPlaceholder: string
   onInputChange: (value: string) => void
+  forwardedRef?:
+    | ((instance: RNTextInput | null) => void)
+    | React.MutableRefObject<RNTextInput | null>
+    | null
 }
 
-type Props = SingleDigitInputProps
+type Props = Omit<RNTextInputProps, 'style'> & SingleDigitInputProps
 
-export function SingleDigitInput({ inputValue, inputPlaceholder, onInputChange }: Props) {
+function SingleDigitTextInput({
+  inputValue,
+  inputPlaceholder,
+  onInputChange,
+  forwardedRef,
+}: Props) {
   return (
     <TextInput
       value={inputValue}
@@ -21,9 +34,17 @@ export function SingleDigitInput({ inputValue, inputPlaceholder, onInputChange }
       maxLength={1}
       showClearButton={false}
       style={styles.codeInput}
+      inputStyle={styles.inputText}
+      ref={forwardedRef}
     />
   )
 }
+
+const SingleDigitInput = React.forwardRef<RNTextInput, Props>((props, ref) => {
+  return <SingleDigitTextInput {...props} forwardedRef={ref} />
+})
+
+export default SingleDigitInput
 
 const styles = StyleSheet.create({
   codeInput: {
@@ -34,5 +55,8 @@ const styles = StyleSheet.create({
     height: 50,
     width: 50,
     marginVertical: 5,
+  },
+  inputText: {
+    textAlign: 'center',
   },
 })
